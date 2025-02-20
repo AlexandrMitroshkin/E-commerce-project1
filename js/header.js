a412034d5e0445ed219d10fc7847f320afbd5ce0
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2025-02-20 11:40:53 +0300

    comit16_before_heder_js_update

--- js/header.js
+++ js/header.js
@@ -4,7 +4,6 @@ let headerRegister = document.querySelector(".header-register");
 
 headerRegisterClose.onclick = function(){
   headerRegister.style.display = "none";
-  // headerRegister.classList.add('hidden');
   header.style.paddingTop = "10px" 
   header.style.height = "48px"
 }
@@ -17,10 +16,13 @@ let headerNavLinks = document.querySelector(".header-nav-links");
 headerNavMore.onclick = function(){
   headerNavLinks.classList.toggle("visible");
   headerNavMore.classList.toggle("inverted");
+  if (headerNavLinks.classList.contains("visible")){
+    header.style.marginBottom = "60px"
+  }
 }
 
-                                            
-                                            // элеметнты выпадающего списка
+
+                                            // реализация выпадающего списка
 
 let header = document.getElementById("header");
 let selectedOptionArrow = document.querySelector(".selected-option-arrow");
@@ -28,7 +30,40 @@ const selectedOption = document.getElementById('selected-option');
 const optionsContainer = document.querySelector('.options');
 const optionsList = document.querySelectorAll('.option');
 
-                                            // элементы search
+function toggleOptions() {
+  if (optionsContainer.classList.contains('visible')) {
+    optionsContainer.classList.remove('visible');
+    selectedOptionArrow.classList.add("notInverted");
+    if (!headerNavLinks.classList.contains('visible')) {
+      header.style.marginBottom = "24px";
+    }else if(headerNavLinks.classList.contains('visible')){
+      header.style.marginBottom = "148px";
+    }
+    else{
+      header.style.marginBottom = "100px";
+    }
+  } else {
+    optionsContainer.classList.add('visible');
+    selectedOptionArrow.classList.remove("notInverted");
+  }
+}
+
+selectedOption.onclick = toggleOptions;
+
+document.onclick = function(event){
+  if (!event.target.closest('.select-wrapper')) {
+    optionsContainer.classList.remove('visible');
+  if (!headerNavLinks.classList.contains('visible') && !optionsContainer.classList.contains('visible')) {
+    header.style.marginBottom = "24px";
+  }else if(headerNavLinks.classList.contains('visible')){
+    header.style.marginBottom = "60px";
+  }else if (headerNavLinks.classList.contains('visible') && optionsContainer.classList.contains('visible')){
+    header.style.marginBottom = "148px";
+  }
+  }
+}
+
+                                                        // реализация search
 
 const input = document.getElementById('header-nav-input');
 const customDatalist = document.getElementById('custom-datalist');
@@ -36,7 +71,7 @@ let headerNavSearch = document.querySelector(".header-nav-search");
 
 const data = {
   "thing1": "https://trychatgpt.ru/",
-  "thing2": "#",
+  "thing2": "https://github.com/",
   "thing3": "#",
   "thing4": "#",
   "thing5": "#",
@@ -47,35 +82,6 @@ const data = {
   "thing10": "#"
 };
 
-
-                                            // реализация выпадающего списка
-
-function toggleOptions() {
-  if (optionsContainer.classList.contains('visible')) {
-    optionsContainer.classList.remove('visible');
-    selectedOptionArrow.classList.add("notInverted");
-    if (!customDatalist.classList.contains('show')) {
-      header.style.marginBottom = "24px";
-    }
-  } else {
-    optionsContainer.classList.add('visible');
-    header.style.marginBottom = "100px";
-    selectedOptionArrow.classList.remove("notInverted");
-  }
-}
-
-selectedOption.addEventListener('click', toggleOptions);
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.select-wrapper')) {
-        optionsContainer.classList.remove('visible');
-      if (!customDatalist.classList.contains('show')) {
-        header.style.marginBottom = "24px";
-      }
-    }
-  });
-
-                                                        // реализация search
-                                                        
   function updateDatalist() {
   const inputValue = input.value.toLowerCase();
   const filteredData = Object.keys(data).filter(item => {
@@ -86,17 +92,18 @@ document.addEventListener('click', (event) => {
 
   if (inputValue && filteredData.length > 0) {
     filteredData.forEach(item => {
-      const option = document.createElement('div');
+      const option = document.createElement('a');
       option.textContent = item;
-      option.setAttribute('data-href', data[item]);
-      option.addEventListener('click', () => {
+      option.setAttribute('href', data[item]);
+      option.onclick = function(){
         input.value = item;
         customDatalist.classList.remove('show');
-        if (!optionsContainer.classList.contains('visible')) {
+        if (!optionsContainer.classList.contains('visible') && !headerNavLinks.classList.contains('visible')) {
           header.style.marginBottom = "24px";
+        }else{
+          header.style.marginBottom = "60px";
         }
-        window.location.href = data[item];
-      });
+      };
       customDatalist.appendChild(option);
     });
 
@@ -123,8 +130,10 @@ document.addEventListener('click', (event) => {
     customDatalist.classList.add('show');
   } else {
     customDatalist.classList.remove('show');
-    if (!optionsContainer.classList.contains('visible')) {
+    if (!optionsContainer.classList.contains('visible') && !headerNavLinks.classList.contains('visible')) {
       header.style.marginBottom = "24px";
+    }else if (headerNavLinks.classList.contains('visible')){
+      header.style.marginBottom = "60px";
     }
   }
 }
@@ -134,8 +143,15 @@ input.addEventListener('input', updateDatalist);
 document.addEventListener('click', (event) => {
   if (!event.target.closest('.header-nav-search')) {
     customDatalist.classList.remove('show');
-    if (!optionsContainer.classList.contains('visible')) {
+    if (!optionsContainer.classList.contains('visible') && !headerNavLinks.classList.contains('visible')) {
       header.style.marginBottom = "24px";
+    }else if (!optionsContainer.classList.contains('visible') && headerNavLinks.classList.contains('visible')){
+      header.style.marginBottom = "60px";
+    }else if (optionsContainer.classList.contains('visible') && headerNavLinks.classList.contains('visible')){
+      header.style.marginBottom = "148px";
+    }
+    else if (optionsContainer.classList.contains('visible') && !headerNavLinks.classList.contains('visible')){
+      header.style.marginBottom = "100px";
     }
   }
 });
@@ -144,66 +160,23 @@ document.addEventListener('click', (event) => {
 
 
 
-function updateDatalist() {
-    const inputValue = input.value.toLowerCase();
-    const filteredData = Object.keys(data).filter(item => {
-      return item.toLowerCase().includes(inputValue);
-    });
-  
-    customDatalist.innerHTML = '';
-  
-    if (inputValue && filteredData.length > 0) {
-      filteredData.forEach(item => {
-        const option = document.createElement('div');
-        option.textContent = item;
-        option.setAttribute('data-href', data[item]);
-        option.addEventListener('click', () => {
-          input.value = item;
-          customDatalist.classList.remove('show');
-          if (!optionsContainer.classList.contains('visible')) {
-            header.style.marginBottom = "24px";
-          }
-          window.location.href = data[item];
-        });
-        customDatalist.appendChild(option);
-      });
-  
-      switch (filteredData.length) {
-        case 1:
-          if(!optionsContainer.classList.contains('visible')){
-              header.style.marginBottom = "61.5px";
-          }
-          else{
-              header.style.marginBottom = "100px";
-          }
-          break;
-        case 2:
-          header.style.marginBottom = "100px";
-          break;
-        case 3:
-          header.style.marginBottom = "136.5px";
-          break;
-        default:
-          header.style.marginBottom = "174px";
-          break;
-      }
-  
-      customDatalist.classList.add('show');
-    } else {
-      customDatalist.classList.remove('show');
-      if (!optionsContainer.classList.contains('visible')) {
-        header.style.marginBottom = "24px";
-      }
-    }
-  }
-  
-input.addEventListener('input', updateDatalist);
-  
-document.addEventListener('click', (event) => {
-if (!event.target.closest('.header-nav-search')) {
-    customDatalist.classList.remove('show');
-    if (!optionsContainer.classList.contains('visible')) {
-    header.style.marginBottom = "24px";
-    }
-}
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                            
