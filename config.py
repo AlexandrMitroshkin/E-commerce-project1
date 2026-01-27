1c379c9b682ddeddbb37450d41718e012fc7613e
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2026-01-27 15:49:45 +0300

    27.01.25

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 
-# Я плохо  помню что это 
-
 class Config:
     SECRET_KEY = '1NZqsu3leV43JS4o6VlY'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///shop.db'
