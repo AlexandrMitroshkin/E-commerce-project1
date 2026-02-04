f7500654b35b74bb59383fdd2be305c3e3e30aef
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2026-02-04 19:46:07 +0300

    Update SECRET_KEY in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = '1NZqsu3leV43JS4o6VlY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'rBas3mf8956feiWcxjK9qP8UB-hw5K0ho5AZajWQI6U'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///shop.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
