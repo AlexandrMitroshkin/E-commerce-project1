43c7196dcc08a24b321b9a2e131c4da259e9bd44
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2026-02-04 23:22:58 +0300

    Fix render attempt7

--- application.py
+++ application.py
@@ -1,12 +1,8 @@
-"""
-Главный файл приложения для Render
-"""
-import os
-from app import create_app
+п»їfrom app import create_app
 
-# СОЗДАЕМ ПЕРЕМЕННУЮ app - ОНА НУЖНА ДЛЯ GUNICORN!
 app = create_app()
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
